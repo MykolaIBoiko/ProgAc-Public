78975f1aa8488e849020945c84d1a224dc638765
MykolaIBoiko <119815859+MykolaIBoiko@users.noreply.github.com>
2022-12-04 23:26:35 +0200

    Create HW 1 ex 5 30 11.py

---
+++ HW 1 ex 5 30 11.py
@@ -0,0 +1,3 @@
+r = float(input('Input radius of the circle in cm and press enter:'))
+p = 3.14159
+print('The results for circle with such radius are:', '\n', 'diametre =',2*r,'\n','circumference =',2*p*r,'\n','area =',p*(r**2))
