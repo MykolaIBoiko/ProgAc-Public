4aa53f6dafa020c9afcf60c8f2337293a21d01c7
MykolaIBoiko <119815859+MykolaIBoiko@users.noreply.github.com>
2022-12-04 23:26:23 +0200

    Create HW 1 ex 3 30 11.py

---
+++ HW 1 ex 3 30 11.py
@@ -0,0 +1,7 @@
+a = float(input('Input first number and press enter:'))
+b = float(input('Input second number and press enter:'))
+s = a+b
+p = a*b
+d = a-b
+q = a/b
+print('The results for first and second numbers are:', '\n', 'sum =',s,'\n','product =',p,'\n','defference =',d,'\n','quotient =',q)
